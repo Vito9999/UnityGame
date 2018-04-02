5a6f0b92ca53b8766ba2203141491741adda8ab7
ogris <m.ogris1@gmx.at>
2018-04-02 19:32:56 +0200

    update

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,25 +6,18 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
 
-    private Rigidbody2D Rb2d;
-
     private Vector3 actpos;
 
     void Start()
     {
-        Rb2d = GetComponent<Rigidbody2D>();
         actpos = this.transform.position;
-        
     }
 
     void FixedUpdate()
     {
-        float MoveHorizontal = Input.GetAxis("Horizontal");
-        float MoveVertical = Input.GetAxis("Vertical");
-        actpos.x += MoveHorizontal;
-        actpos.y += MoveVertical;
+        actpos.x += Input.GetAxis("Horizontal"); 
+        actpos.y += Input.GetAxis("Vertical"); 
         this.transform.SetPositionAndRotation(actpos, new Quaternion(0, 0, 0, 0));
-
     }
    
     
