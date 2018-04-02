f7959c5a0b0e0f644bb2c2973ab8b56708b93782
Vito9999 <vitoelite@gmail.com>
2018-04-02 18:25:08 +0200

    inet

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody2D Rb2d;
+    
+
+
+    void Start()
+    {
+        Rb2d = GetComponent<Rigidbody2D>();
+    
+        
+    }
+
+    void FixedUpdate()
+    {
+        float MoveHorizontal = Input.GetAxis("Horizontal");
+        float MoveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(MoveHorizontal, MoveVertical);
+        Rb2d.AddForce(movement * 10);
+
+        
+    }
+   
+    
+
+
+}
---
+++ Assets/Select.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Select : MonoBehaviour {
+
+    public GameObject[] objects;
+    SpriteRenderer  rend;
+    /*public class enemie
+    {
+      
+    }
+    enemie E1 = new enemie();*/
+    private void Start()
+    {
+       
+        
+        foreach (GameObject select in objects)
+        {
+            rend = select.gameObject.GetComponent<SpriteRenderer>();
+            rend.enabled = false;
+            print(this.gameObject.name + "  selected: " + rend.enabled);
+        }
+    }
+    
+
+    // Update is called once per frame
+    private void OnMouseDown()
+    {
+
+        foreach (GameObject select in objects)
+        {
+            rend = select.gameObject.GetComponent<SpriteRenderer>();
+            rend.enabled = !rend.enabled;
+
+        }
+        print(this.gameObject.name + "  selected: " + rend.enabled);
+    }
+       
+    
+   
+    
+}
